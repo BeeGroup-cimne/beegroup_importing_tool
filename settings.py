67e0965936d6c9d42d2c3f9a2b647a9543ae7f93
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2022-02-17 16:40:06 +0100

    corrected settings.py

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 conf_file = os.getenv("CONFIG_FILE")
-kafka_message_size = os.getenv("KAFKA_MESSAGE_SIZE")
+kafka_message_size = int(os.getenv("KAFKA_MESSAGE_SIZE"))
 secret_password = os.getenv("SECRET_PASSWORD")
