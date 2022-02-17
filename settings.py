192e1e16c528826484216d5f6f0b858d940bccc7
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2022-02-17 13:20:08 +0100

    add loadenv

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 conf_file = os.getenv("CONFIG_FILE")
 kafka_message_size = os.getenv("KAFKA_MESSAGE_SIZE")
 secret_password = os.getenv("SECRET_PASSWORD")
