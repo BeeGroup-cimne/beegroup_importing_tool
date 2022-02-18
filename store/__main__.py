eaf8e51f7b9dc08cdf6e0d2405265aaf5eb3ee96
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2022-02-18 15:52:01 +0100

    avoid kafka commit error in store

--- store/__main__.py
+++ store/__main__.py
@@ -8,9 +8,11 @@ if __name__ == '__main__':
     config = read_config(settings.conf_file)
     sources_available = load_plugins()
     consumer = read_from_kafka(config['kafka']['topic'], config['kafka']['group_store'], config['kafka']['connection'])
-    for m in consumer:
-        m.commit()
-        print("processing_message")
+    while True:
+        consumer.resume()
+        m = consumer.next_v2()
+        consumer.commit()
+        consumer.pause()
         message = m.value
         if 'logger' in message:
             mongo_logger.import_log(message['logger'], "store")
@@ -18,7 +20,7 @@ if __name__ == '__main__':
         if 'message_part' in message:
             message_part = message['message_part']
 
-        log_string(f"received part {message_part} from {message['source']} to store")
+        # log_string(f"received part {message_part} from {message['source']} to store")
         table = None
         for k, v in sources_available.items():
             if message['source'] == k:
@@ -29,6 +31,6 @@ if __name__ == '__main__':
             try:
                 save_to_hbase(message['data'], table, config['hbase_store_raw_data'], [("info", "all")],
                               row_fields=message['row_keys'])
-                mongo_logger.log(f"part {message_part} successfully stored to HBASE")
+                log_string(f"part {message_part} successfully stored to HBASE")
             except Exception as e:
-                mongo_logger.log(f"error storing part {message_part} to HBASE: {e}")
+                log_string(f"error storing part {message_part} to HBASE: {e}")
