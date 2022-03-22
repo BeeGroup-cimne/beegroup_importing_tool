96697bb4539b1011868b9a185fde8a2380c75723
elskater98 <elskater98@gmail.com>
2022-03-22 14:02:16 +0100

    Cast datetime64[ns] to unix timestamp

--- sources/Nedgia/harmonizer/mapper_ts.py
+++ sources/Nedgia/harmonizer/mapper_ts.py
@@ -1,9 +1,30 @@
 import pandas as pd
+from neo4j import GraphDatabase
+from rdflib import Namespace
 
 
 def harmonize_data(data, **kwargs):
-    df = pd.DataFrame.from_records(data)
-    df['Fecha inicio Docu. cálculo'] = df['Fecha inicio Docu. cálculo'].dt.strftime('%Y/%m/%d 00:%M:%S')  # ISO 8601
-    df['Fecha fin Docu. cálculo'] = df['Fecha fin Docu. cálculo'].dt.strftime('%Y/%m/%d 23:%M:%S')  # ISO 8601
+    namespace = kwargs['namespace']
+    user = kwargs['user']
+    config = kwargs['config']
 
-    df = df[['CUPS', 'Fecha inicio Docu. cálculo', 'Fecha fin Docu. cálculo', 'Consumo kWh ATR', 'Consumo kWh GLP']]
+    hbase_conn2 = config['hbase_store_harmonized_data']
+    neo4j_connection = config['neo4j']
+
+    neo = GraphDatabase.driver(**neo4j_connection)
+    n = Namespace(namespace)
+
+    df = pd.DataFrame.from_records(data)
+
+    df['Fecha inicio Docu. cálculo'] = pd.to_datetime(
+        df['Fecha inicio Docu. cálculo'].dt.strftime('%Y/%m/%d 00:%M:%S'))  # ISO 8601
+    df['Fecha fin Docu. cálculo'] = pd.to_datetime(
+        df['Fecha fin Docu. cálculo'].dt.strftime('%Y/%m/%d 23:%M:%S'))  # ISO 8601
+
+    df['ts_init'] = df['Fecha inicio Docu. cálculo'].astype('int') / 10 ** 9
+    df['ts_init'] = df['ts_init'].astype('int')
+
+    df['ts_end'] = df['Fecha fin Docu. cálculo'].astype('int') / 10 ** 9
+    df['ts_end'] = df['ts_end'].astype('int')
+
+    print(df)
