d41cdffe11c3495baf96f4a1fa8d878c2b7f8be9
elskater98 <elskater98@gmail.com>
2022-04-22 16:33:16 +0200

    -

--- sources/BulgariaSummary/harmonizer/__init__.py
+++ sources/BulgariaSummary/harmonizer/__init__.py
@@ -1,2 +1,37 @@
+from neo4j import GraphDatabase
+from rdflib import Namespace
+import pandas as pd
+
+
 def harmonize_data(data, **kwargs):
-    pass
+    df = pd.DataFrame().from_records(data)
+    df['type_of_building'] = df['type_of_building'].str.strip()
+
+    # Match Taxonomy
+
+    tax_df = pd.read_excel("data/tax/TAX_BULGARIA.xlsx", header=None, names=["Source", "Taxonomy"], sheet_name='Hoja1')
+
+    tax_dict = {}
+    for i in tax_df.to_dict(orient="records"):
+        tax_dict.update({i['Source']: i['Taxonomy']})
+
+    df['type_of_building'] = df['type_of_building'].map(tax_dict)
+
+    # Variables
+    namespace = kwargs['namespace']
+    user = kwargs['user']
+    config = kwargs['config']
+
+    # Database connections
+
+    neo4j_connection = config['neo4j']
+    neo = GraphDatabase.driver(**neo4j_connection)
+
+    # Building
+    # EnergyEfficiencyMeasurement
+    # Location
+    # EnergySavings
+
+    with neo.session() as session:
+        n = Namespace(namespace)
+        bulding_uri = n[f""]
--- utils/utils.py
+++ utils/utils.py
@@ -10,6 +10,7 @@ def load_plugins(settings):
     sources_available = {}
     for finder, name, is_pkg in pkgutil.iter_modules(['sources']):
         source_module = importlib.import_module(f"{finder.path}.{name}")
+        # source_module = importlib.import_module(f"sources.{name}")
         source = source_module.Plugin(settings=settings)
         sources_available[source.source_name] = source
     return sources_available
