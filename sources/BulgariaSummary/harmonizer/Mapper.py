8b05ae693c8dd7bff3f867b1dd4d61ceb9fef294
elskater98 <elskater98@gmail.com>
2022-04-27 17:02:22 +0200

    BuildingSpace and Area

--- sources/BulgariaSummary/harmonizer/Mapper.py
+++ sources/BulgariaSummary/harmonizer/Mapper.py
@@ -1,7 +1,10 @@
+from functools import partial
+
 from slugify import slugify
 
-from utils.data_transformations import building_subject, location_info_subject, building_department_subject
-from utils.rdf_utils.big_classes import Organization, Building, LocationInfo
+from utils.data_transformations import building_subject, location_info_subject, building_department_subject, \
+    building_space_subject, gross_area_subject
+from utils.rdf_utils.big_classes import Organization, Building, LocationInfo, BuildingSpace, Area
 from utils.rdf_utils.bigg_definition import Bigg
 
 
@@ -11,6 +14,8 @@ class Mapper(object):
         Organization.set_namespace(namespace)
         Building.set_namespace(namespace)
         LocationInfo.set_namespace(namespace)
+        BuildingSpace.set_namespace(namespace)
+        Area.set_namespace(namespace)
 
     def get_mappings(self, group):
         organization = {
@@ -88,6 +93,41 @@ class Mapper(object):
                         "operations": []
                     },
                 }
+            },
+            "links": {
+                "building_space": {
+                    "type": Bigg.hasSpace,
+                    "link": "subject"
+                },
+                "location_info": {
+                    "type": Bigg.hasLocationInfo,
+                    "link": "subject"
+                }
+            }
+        }
+
+        building_space = {
+            "name": "building_space",
+            "class": BuildingSpace,
+            "type": {
+                "origin": "row"
+            },
+            "params": {
+                "raw": {
+                    "buildingSpaceName": "Building",
+                },
+                "mapping": {
+                    "subject": {
+                        "key": "subject",
+                        "operations": [building_space_subject]
+                    }
+                }
+            },
+            "links": {
+                "gross_floor_area": {
+                    "type": Bigg.hasAreas,
+                    "link": "subject"
+                }
             }
         }
 
@@ -114,8 +154,32 @@ class Mapper(object):
             }
         }
 
+        gross_floor_area = {
+            "name": "gross_floor_area",
+            "class": Area,
+            "type": {
+                "origin": "row"
+            },
+            "params": {
+                "raw": {
+                    "areaType": "GrossFloorArea",
+                    "areaUnitOfMeasurement": "m2",
+                },
+                "mapping": {
+                    "subject": {
+                        "key": "subject",
+                        "operations": [partial(gross_area_subject, a_source=self.source)]
+                    },
+                    "areaValue": {
+                        "key": "gross_floor_area",
+                        "operations": []
+                    }
+                }
+            }
+        }
+
         grouped_modules = {
-            "all": [organization, building_organization, buildings, location_info]
+            "all": [organization, building_organization, buildings, location_info, building_space, gross_floor_area]
         }
 
         return grouped_modules[group]
