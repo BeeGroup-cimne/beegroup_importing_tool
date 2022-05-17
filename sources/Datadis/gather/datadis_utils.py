0b67bb6fad51aaf0e7c2ba8fc54aefaa17d18956
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2022-05-17 15:42:19 +0200

    corrected password decrypt for datadis

--- sources/Datadis/gather/datadis_utils.py
+++ sources/Datadis/gather/datadis_utils.py
@@ -8,7 +8,7 @@ bigg = settings.namespace_mappings['bigg']
 
 def decrypt_passwords(users, settings):
     for user in users:
-        enc_dict = ast.literal_eval(user['password'])
+        enc_dict = user['password']
         user['password'] = utils.security.decrypt(enc_dict, settings.secret_password)
     return users
 
@@ -19,7 +19,7 @@ def get_users(neo4j):
         users = session.run(
             f"""Match (n:DatadisSource)<-[:hasSource]-(o:{bigg}__Organization) 
             CALL{{With o Match (o)<-[:{bigg}__hasSubOrganization*0..]-(d:{bigg}__Organization) WHERE NOT (d)<-[:{bigg}__hasSubOrganization]-() return d}}
-             return n.username AS username, n.password AS password, 
+             return n.username AS username, n.Password AS password, 
              d.userID AS user, split(d.uri,"#")[0]+'#'AS namespace
             """).data()
     return users
