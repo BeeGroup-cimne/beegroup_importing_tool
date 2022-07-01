5fe4fec60b7ac376cd22416ce6d2d938a09aea08
elskater98 <elskater98@gmail.com>
2022-07-01 13:56:17 +0200

    add new gmao endpoints

--- utils/gmao.py
+++ utils/gmao.py
@@ -33,13 +33,13 @@ class GMAO:
         self.session_id = ""
         self.headers = {"Content-Type": "application/json"}
 
-    def find_zones(self, modified_from_date, page_size=100, page_index=0, zone_path=""):
+    def find_zones(self, modified_from_date, page_size=100, page_index=0, zone_path=None):
         body = {"pagesize": page_size,
                 "pageindex": page_index,
                 "modifiedfromdate": modified_from_date,
                 }
 
-        if zone_path != "":
+        if zone_path is not None:
             body.update({"zonepath": zone_path})
 
         body = json.dumps(body)
@@ -88,8 +88,8 @@ class GMAO:
         assert res.status_code == 200
         return res.json()
 
-    def find_indicator_values(self, fromdate, searchtext=None, todate=None, indicatorid=None,
-                              managedscopeid=None, zoneid=None, zonepath=None, pagesize=100, pageindex=0, service=None):
+    def find_indicator_values(self, service, fromdate, searchtext=None, todate=None, indicatorid=None,
+                              managedscopeid=None, zoneid=None, zonepath=None, pagesize=100, pageindex=0):
         body = {}
         if service is None:
             service = ['Maintenance', 'Cleaning', 'Gardening']
@@ -115,16 +115,67 @@ class GMAO:
         if zonepath is not None:
             body.update({"zonepath": zonepath})
 
-        body = {
-            "service": service,
-            "pagesize": pagesize,
-            "pageindex": pageindex,
-        }
+        if zonepath is not None:
+            body.update({"zonepath": zonepath})
+
+        body.update(
+            {
+                "service": service,
+                "pagesize": pagesize,
+                "pageindex": pageindex,
+            })
+
+        body = json.dumps(body)
+        res = requests.post(url=f"{API_URL}/api/workorder/apifindindicatorvalues", headers=self.headers, data=body)
+        assert res.status_code == 200
+        return res.json()
+
+    def find_work_orders(self, service, modifiedfromdate=None, fromorderdate=None, searchtext=None, toorderdate=None,
+                         indicatorid=None,
+                         zoneid=None, zonepath=None, pagesize=100, pageindex=0,
+                         statusids=None, worktypes=None):
+        body = {}
+
+        if modifiedfromdate is not None:
+            body.update({"modifiedfromdate": modifiedfromdate})
+
+        if fromorderdate is not None:
+            body.update({"fromorderdate": fromorderdate})
+
+        if searchtext is not None:
+            body.update({"searchtext": searchtext})
+
+        if toorderdate is not None:
+            body.update({"toorderdate": toorderdate})
+
+        if indicatorid is not None:
+            body.update({"indicatorid": indicatorid})
+
+        if zoneid is not None:
+            body.update({"zoneid": zoneid})
 
         if zonepath is not None:
             body.update({"zonepath": zonepath})
 
+        if statusids is not None:
+            body.update({"statusids": statusids})
+
+        if worktypes is not None:
+            body.update({"worktypes": worktypes})
+
+        body.update({"pagesize": pagesize, "pageindex": pageindex, "service": service})
+
         body = json.dumps(body)
-        res = requests.post(url=f"{API_URL}/api/workorder/apifindindicatorvalues", headers=self.headers, data=body)
+
+        res = requests.post(url=f"{API_URL}/api/workorder/apifindworkorders", headers=self.headers, data=body)
+        assert res.status_code == 200
+        return res.json()
+
+    def get_full_work_order(self, _id, getelementlist=True, gettrasklist=True, gettimelist=True, getfeatures=True):
+        body = json.dumps(
+            {"id": _id, "getfeatures": getfeatures, "getelementlist": getelementlist, "gettrasklist": gettrasklist,
+             "gettimelist": gettimelist})
+
+        res = requests.post(url=f"{API_URL}/api/workorder/apigetfullworkorder", headers=self.headers, data=body)
         assert res.status_code == 200
         return res.json()
