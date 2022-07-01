ef3030d28553c741dd8c9bf6763137e3d9b6422a
elskater98 <elskater98@gmail.com>
2022-07-01 13:11:59 +0200

    add new gmao endpoints

--- utils/gmao.py
+++ utils/gmao.py
@@ -7,6 +7,9 @@ API_URL = "https://host.manttest.net/MTW_InfraestructuresCAT"
 
 class GMAO:
     def __init__(self, username, password, project):
+        self.username = username
+        self.password = password
+        self.project = project
         self.session_id = self.authenticate(username, password, project)
         self.headers = {"Content-Type": "application/json",
                         "x-manttest-loginid": self.session_id}
@@ -19,13 +22,18 @@ class GMAO:
         assert res.status_code == 200
         return res.json()
 
+    def login(self):
+        self.session_id = self.authenticate(self.username, self.password, self.project)
+        self.headers = {"Content-Type": "application/json",
+                        "x-manttest-loginid": self.session_id}
+
     def logout(self):
         body = json.dumps({})
         requests.post(url=f"{API_URL}/api/loginservice/logout", headers=self.headers, data=body)
         self.session_id = ""
         self.headers = {"Content-Type": "application/json"}
 
-    def find_zones(self, page_size, page_index, modified_from_date, zone_path=""):
+    def find_zones(self, modified_from_date, page_size=100, page_index=0, zone_path=""):
         body = {"pagesize": page_size,
                 "pageindex": page_index,
                 "modifiedfromdate": modified_from_date,
@@ -56,5 +64,67 @@ class GMAO:
         })
 
         res = requests.post(url=f"{API_URL}/api/zone/apigetfullzone", headers=self.headers, data=body)
+        assert res.status_code == 200
+        return res.json()
+
+    def find_assets(self, modified_from_date, page_size=100, page_index=0, zone_path=""):
+        body = {"pagesize": page_size,
+                "pageindex": page_index,
+                "modifiedfromdate": modified_from_date,
+                }
+
+        if zone_path != "":
+            body.update({"zonepath": zone_path})
+
+        body = json.dumps(body)
+        res = requests.post(url=f"{API_URL}/api/inventory/apifindassets", headers=self.headers, data=body)
+        assert res.status_code == 200
+        return res.json()
+
+    def get_full_asset(self, _id, get_feature=True):
+        body = json.dumps({"id": _id, "getfeatures": get_feature})
+
+        res = requests.post(url=f"{API_URL}/api/inventory/apigetfullasset", headers=self.headers, data=body)
+        assert res.status_code == 200
+        return res.json()
+
+    def find_indicator_values(self, fromdate, searchtext=None, todate=None, indicatorid=None,
+                              managedscopeid=None, zoneid=None, zonepath=None, pagesize=100, pageindex=0, service=None):
+        body = {}
+        if service is None:
+            service = ['Maintenance', 'Cleaning', 'Gardening']
+
+        if searchtext is not None:
+            body.update({"searchtext": searchtext})
+
+        if todate is not None:
+            body.update({"todate": todate})
+
+        if fromdate is not None:
+            body.update({"fromdate": fromdate})
+
+        if indicatorid is not None:
+            body.update({"indicatorid": indicatorid})
+
+        if managedscopeid is not None:
+            body.update({"managedscopeid": managedscopeid})
+
+        if zoneid is not None:
+            body.update({"zoneid": zoneid})
+
+        if zonepath is not None:
+            body.update({"zonepath": zonepath})
+
+        body = {
+            "service": service,
+            "pagesize": pagesize,
+            "pageindex": pageindex,
+        }
+
+        if zonepath is not None:
+            body.update({"zonepath": zonepath})
+
+        body = json.dumps(body)
+        res = requests.post(url=f"{API_URL}/api/workorder/apifindindicatorvalues", headers=self.headers, data=body)
         assert res.status_code == 200
         return res.json()
