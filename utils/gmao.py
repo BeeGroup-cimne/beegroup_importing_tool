4b8f1148e1ac887e3b82f84977163da322695f60
elskater98 <elskater98@gmail.com>
2022-07-01 12:24:33 +0200

    add new gmao endpoints

--- utils/gmao.py
+++ utils/gmao.py
@@ -1,33 +1,60 @@
+import json
+
 import requests
 
-API_URL = ""
+API_URL = "https://host.manttest.net/MTW_InfraestructuresCAT"
 
 
 class GMAO:
     def __init__(self, username, password, project):
         self.session_id = self.authenticate(username, password, project)
         self.headers = {"Content-Type": "application/json",
-                        "X-ManTest-LoginId": self.session_id}
+                        "x-manttest-loginid": self.session_id}
 
     @staticmethod
     def authenticate(username, password, project):
         headers = {"Content-Type": "application/json"}
-        body = {"username": username, "password": password, "project": project}
-        res = requests.post(url=f"{API_URL}/api/loginservice/LoginUser", headers=headers, data=body)
+        body = json.dumps({"username": username, "password": password, "project": project})
+        res = requests.post(url=f"{API_URL}/api/loginservice/loginuser", headers=headers, data=body)
         assert res.status_code == 200
         return res.json()
 
     def logout(self):
-        body = {}
-        requests.post(url=f"{API_URL}/api/loginservice/Logout", headers=self.headers, data=body)
+        body = json.dumps({})
+        requests.post(url=f"{API_URL}/api/loginservice/logout", headers=self.headers, data=body)
         self.session_id = ""
+        self.headers = {"Content-Type": "application/json"}
 
-    def find_zones(self, page_size, page_index, modified_from_date, zone_path):
+    def find_zones(self, page_size, page_index, modified_from_date, zone_path=""):
         body = {"pagesize": page_size,
                 "pageindex": page_index,
                 "modifiedfromdate": modified_from_date,
-                "zonepath": zone_path
                 }
-        res = requests.post(url=f"{API_URL}/api/zone/APIFindZones", headers=self.headers, data=body)
+
+        if zone_path != "":
+            body.update({"zonepath": zone_path})
+
+        body = json.dumps(body)
+        res = requests.post(url=f"{API_URL}/api/zone/apifindzones", headers=self.headers, data=body)
+        assert res.status_code == 200
+        return res.json()
+
+    def get_full_zone(self, _id, get_criticalities=True, get_managed_scopes=True, get_operations=True,
+                      get_features=True,
+                      services=None):
+
+        if services is None:
+            services = ['Maintenance', 'Cleaning', 'Gardening']
+
+        body = json.dumps({
+            "id": _id,
+            "getcriticalities": get_criticalities,
+            "getmanagedscopes": get_managed_scopes,
+            "getoperations": get_operations,
+            "getfeatures": get_features,
+            "services": services,
+        })
+
+        res = requests.post(url=f"{API_URL}/api/zone/apigetfullzone", headers=self.headers, data=body)
         assert res.status_code == 200
         return res.json()
