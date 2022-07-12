b45631376013da29759a337ed502b7264f3505fa
elskater98 <elskater98@gmail.com>
2022-07-12 10:48:30 +0200

    check columns and translate according to english name

--- sources/ePlanet/gather/__init__.py
+++ sources/ePlanet/gather/__init__.py
@@ -2,15 +2,24 @@ import argparse
 
 import pandas as pd
 
-COLUMN_NAMES = ['Year', 'Month', 'Code', 'Municipality Unit', 'Municipality', 'Region', 'Office', 'Meter id',
+COLUMN_NAMES = ['Year', 'Month', 'Code', 'Municipality Unit', 'Municipality', 'Region', 'Office', 'Meter num',
                 'Bill num', 'Bill num 2', 'Name', 'Street', 'Street num', 'City', 'Bill Issuing Day', 'Month1', 'Year1',
                 'Meter Code', 'Type Of Building', 'Current Record', 'Previous Record', 'Variable', 'Recording Date',
-                'Previous Recording Date', 'Electricity Consumtion', 'Municiaplity Unit 1']
+                'Previous Recording Date', 'Electricity Consumption', 'Electricity Cost', 'VAT', 'Other',
+                'Prev payment',
+                'Energy Value', 'VAT Prev Payment', 'EPT', 'Out service' 'Debit/Credit' 'ETMEAR' 'VAT ETMEAR',
+                'Special TAX',
+                'TAX',
+                'Low VAT',
+                'High VAT', 'Intermediate Value', 'Total Energy Value', 'Total VAT of electricity',
+                'Total VAT Services', 'Total VAT', 'Total ERT', 'Municipal TAX', 'Total TAP',
+                'EETIDE', 'Total Account', 'Total Current Month', 'Account Type',
+                'Municiaplity Unit 1']
 
 
 def gather_data(config, settings, args):
     df = pd.read_excel('data/ePlanet/sample of the translations in the energy data of the municipality .xlsx',
-                       skiprows=5)
+                       skiprows=7, header=None, names=COLUMN_NAMES, index_col=None)
 
 
 def gather(arguments, config=None, settings=None):
