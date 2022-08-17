388cd1d64a5690db72338f8b256b53d75c6e8006
elskater98 <elskater98@gmail.com>
2022-08-17 16:02:40 +0200

    Gather supplies and reads

--- sources/DEXMA/gather/__init__.py
+++ sources/DEXMA/gather/__init__.py
@@ -1,35 +1,70 @@
 import argparse
+from enum import Enum
 
+import pandas as pd
 from dexma.device import Device
+from dexma.location import Location
+from dexma.reading import Reading
+from dexma.supply import Supply
 
 from utils.hbase import save_to_hbase
 from utils.kafka import save_to_kafka
-from utils.nomenclature import raw_nomenclature, RAW_MODE
+from utils.nomenclature import raw_nomenclature
 from utils.utils import log_string
 
 
-def gather_devices(config, settings, args):
-    d = Device()
+class SupplyEnum(Enum):
+    WATER = 'WATER',
+    ELECTRICITY = 'ELECTRICITY'
+    GAS = 'GAS'
+
+
+def gather_static_data(config, settings, args):
     count = 0
     limit = 500
 
     while True:
-        res = d.get_devices({"start": limit * count, "limit": limit})
+        devices = Device().get_devices({"start": count * limit, "limit": limit}).json()
 
-        try:
-            assert res.status_code == 200
-        except Exception as ex:
-            log_string(f"{ex}", mongo=False)
-            continue
+        for index, row in pd.DataFrame(devices).iterrows():
+            if row['location']:
+                location = gather_location(row['location']['id']).json()
 
-        save_data(data=res.json(), data_type='Devices',
-                  row_keys=['id'], column_map=[("info", "all")],
-                  config=config, settings=settings, args=args, raw_mode=RAW_MODE.STATIC)
-
-        if len(res.json()) == limit:
+        if len(devices) == limit:
             count += 1
         else:
             break
+
+
+def gather_reads(device_id, date_init, date_end):
+    try:
+        r = Reading()
+        res = r.get_readings_by_parameter_key({"device_id": device_id,
+                                               "parameter_key": 'CURRENT',
+                                               "operation": "RAW",
+                                               "resolution": 'H',
+                                               "from": date_init,
+                                               "to": date_end})
+        return res
+    except Exception as ex:
+        print(f"{ex}")
+
+
+def gather_supplies(supply_type: SupplyEnum):
+    limit = 20
+    count = 0
+    while True:
+        supplies = Supply().get_energy_source_supplies(supply_type.value,
+                                                       {"start": count * limit, "limit": limit}).json()
+
+        if len(supplies) == limit:
+            count += 1
+        else:
+            break
+
+
+def gather_location(id):
+    return Location().get_location(id)
 
 
 def save_data(data, data_type, row_keys, column_map, config, settings, args, raw_mode):
@@ -74,5 +109,5 @@ def gather(arguments, settings, config):
                     choices=["devices", "location", "parameter", "supplies", "reading", "all"])
     args = ap.parse_args(arguments)
 
-    if args.kind_of_data == "devices" or args.kind_of_data == "all":
-        gather_devices(arguments, settings, config)
+    # if args.kind_of_data == "devices" or args.kind_of_data == "all":
+    #     gather_devices(arguments, settings, config)
