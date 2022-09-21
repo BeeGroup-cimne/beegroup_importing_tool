ec107b8dfd3c5dc3f6e9fd3047129b3011b77258
elskater98 <elskater98@gmail.com>
2022-09-21 11:52:26 +0200

    Gather data from OpenData

--- sources/OpenData/__init__.py
+++ sources/OpenData/__init__.py
@@ -1,5 +1,6 @@
 from sources import SourcePlugin
 from sources.OpenData.gather import gather
+from utils.nomenclature import raw_nomenclature, RAW_MODE
 
 
 class Plugin(SourcePlugin):
@@ -8,27 +9,23 @@ class Plugin(SourcePlugin):
     def gather(self, arguments):
         gather(arguments, settings=self.settings, config=self.config)
 
-    def harmonizer_command_line(self, arguments):
-        # harmonize_command_line(arguments, config=self.config, settings=self.settings)
-        pass
-
     def get_mapper(self, message):
-        pass
-        # if message["collection_type"] == 'devices':
-        #     return harmonize_data_device
-        # elif message["collection_type"] == 'invoices':
-        #     return harmonize_data_ts
+        if message["collection_type"] == 'EnergyPerformanceCertificate':
+            return harmonize_data_device
+
 
     def get_kwargs(self, message):
         return {
             "namespace": message['namespace'],
             "user": message['user'],
             "config": self.config,
-            "timezone": message['timezone']
         }
 
     def get_store_table(self, message):
-        if message['collection_type'] == "devices":
+        if message['collection_type'] == "EnergyPerformanceCertificate":
+            h_table_name = raw_nomenclature(mode=RAW_MODE.STATIC, data_type=message['collection_type'], frequency="",
+                                            user=message['user'],
+                                            source=self.source_name)
+            return h_table_name
+        else:
             return None  # Useless info
-        elif message['collection_type'] == "invoices":
-            return f"raw_Nedgia_ts_invoices__{message['user']}"
