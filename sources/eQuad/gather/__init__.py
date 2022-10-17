e1c512c4f8faf062055184446c39da7e017d2e61
elskater98 <elskater98@gmail.com>
2022-10-17 12:49:28 +0200

    Gather data from eQuad

--- sources/eQuad/gather/__init__.py
+++ sources/eQuad/gather/__init__.py
@@ -1,8 +1,12 @@
 import argparse
 from json import dumps
 
-import pandas as pd
 import requests
+
+from utils.hbase import save_to_hbase
+from utils.kafka import save_to_kafka
+from utils.nomenclature import raw_nomenclature, RAW_MODE
+from utils.utils import log_string
 
 BASE_URL = "https://staging-c3po.equadcapital.com"
 BASE_HEADERS = {'Content-Type': 'application/json;charset=utf-8'}
@@ -21,10 +25,41 @@ def gather_projects(token):
     response = requests.get(f"{BASE_URL}/api/projects?token={token}")
 
     if response.ok:
-        data = response.json()
-        df = pd.json_normalize(data, sep='_')
+        return response.json()
     else:
         raise Exception(f"Gather project receives a {response.status_code}")
+
+
+def save_data(data, data_type, row_keys, column_map, config, settings, args):
+    if args.store == "kafka":
+        try:
+            k_topic = config["kafka"]["topic"]
+            kafka_message = {
+                "namespace": args.namespace,
+                "user": args.user,
+                "collection_type": data_type,
+                "source": config['source'],
+                "row_keys": row_keys,
+                "column_map": column_map,
+                "data": data
+            }
+            save_to_kafka(topic=k_topic, info_document=kafka_message,
+                          config=config['kafka']['connection'], batch=settings.kafka_message_size)
+
+        except Exception as e:
+            log_string(f"error when sending message: {e}")
+
+    elif args.store == "hbase":
+
+        try:
+            h_table_name = raw_nomenclature(mode=RAW_MODE.STATIC, data_type=data_type, frequency="", user=args.user,
+                                            source=config['source'])
+            save_to_hbase(data, h_table_name, config['hbase_store_raw_data'], column_map,
+                          row_fields=row_keys)
+        except Exception as e:
+            log_string(f"Error saving datadis supplies to HBASE: {e}")
+    else:
+        log_string(f"store {config['store']} is not supported")
 
 
 def gather(arguments, config=None, settings=None):
@@ -35,4 +70,8 @@ def gather(arguments, config=None, settings=None):
     args = ap.parse_args(arguments)
 
     token = get_token(**config['eQuad'])
-    gather_projects(token=token)
+    data = gather_projects(token=token)
+
+    save_data(data=data, data_type='Projects',
+              row_keys=['_id'], column_map=[("info", "all")],
+              config=config, settings=settings, args=args)
