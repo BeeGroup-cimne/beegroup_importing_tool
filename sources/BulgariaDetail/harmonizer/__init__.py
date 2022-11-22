c53994866a807faecf4159b255dbbafe22627a7b
elskater98 <elskater98@gmail.com>
2022-11-22 13:17:12 +0100

    Harmonize eem and es

--- sources/BulgariaDetail/harmonizer/__init__.py
+++ sources/BulgariaDetail/harmonizer/__init__.py
@@ -1,18 +1,13 @@
-import hashlib
 from datetime import timedelta
 
-from neo4j import GraphDatabase
 from rdflib import Namespace
 from slugify import slugify
 
-import settings
-from sources.Bulgaria.constants import enum_energy_efficiency_measurement_type, enum_energy_saving_type
+from sources.Bulgaria.constants import enum_energy_efficiency_measurement_type, enum_energy_saving_type, eem_headers
 from sources.Bulgaria.harmonizer.Mapper import Mapper
 from sources.Bulgaria.harmonizer.mapper_buildings import set_source_id
 from sources.BulgariaDetail.utils import set_taxonomy, set_municipality
 from utils.data_transformations import *
-from utils.neo4j import create_sensor
-from utils.rdf_utils.ontology.namespaces_definition import bigg_enums, units
 from utils.rdf_utils.rdf_functions import generate_rdf
 from utils.rdf_utils.save_rdf import save_rdf_with_source, link_devices_with_source
 
@@ -86,18 +81,45 @@ def clean_project(df):
               [x for x in df.columns if re.match("project_energy_saving_subject_.*", x)]]
 
 
-def clean_dataframe_eem_savings(df, eems_parted, start_column):
+def clean_eem_savings(df, source):
     df['element_subject'] = df['subject'].apply(construction_element_subject)
-    saving_columns = []
-    for i, eem_type in enumerate(eems_parted):
-        list_i = i + start_column
-        df[f"eem_{list_i}_subject"] = df['subject'].apply(lambda x: f"{x}-{eem_type}").apply(eem_subject)
-        for j, e_saving_type in enumerate(enum_energy_saving_type):
-            df_t = df['subject'].apply(lambda x: f"{x}-{eem_type}-{e_saving_type}").apply(energy_saving_subject)
-            df_t.name = f"energy_saving_{list_i}_{j}_subject"
-            saving_columns.append(df_t)
-    df = pd.concat([df] + saving_columns, axis=1)
-    return df[['subject', 'element_subject', 'epc_date_before', 'epc_date'] +
+    df['device_subject'] = df['subject'].apply(partial(device_subject, source=source))
+
+    for i in range(len(enum_energy_efficiency_measurement_type)):
+        for j in range(len(eem_headers)):
+            if j == 0:
+                df[f"measurement_{i}_{eem_headers[j]}"] = df[f"measure_{i}_0"] + df[f"measure_{i}_1"]
+
+            if j == 1:
+                df[f"measurement_{i}_{eem_headers[j]}"] = df[f"measure_{i}_3"]
+
+            if j == 2:
+                df[f"measurement_{i}_{eem_headers[j]}"] = df[f"measure_{i}_4"] + df[f"measure_{i}_5"]
+
+            if j == 3:
+                df[f"measurement_{i}_{eem_headers[j]}"] = df[f"measure_{i}_8"] + df[
+                    f"measure_{i}_2"] + df[
+                                                              f"measure_{i}_6"] + df[f"measure_{i}_7"]
+
+            if j == 4:
+                df[f"measurement_{i}_{eem_headers[j]}"] = df[f"measure_{i}_9"]
+
+            if j == 5:
+                df[f"measurement_{i}_{eem_headers[j]}"] = df[f"measure_{i}_10"]
+
+            if j == 6:
+                df[f"measurement_{i}_{eem_headers[j]}"] = df[f"measure_{i}_11"]
+
+    for i in range(len(enum_energy_efficiency_measurement_type)):
+        df[f"eem_{i}_subject"] = 'EEM-' + df['subject'] + '-' + enum_energy_efficiency_measurement_type[i]
+        df[f"emm_{i}_type"] = enum_energy_efficiency_measurement_type[i]
+
+        for j in range(len(enum_energy_saving_type)):
+            df[f"energy_saving_{i}_{j}_subject"] = 'EnergySaving-' + df['subject'] + '-' + \
+                                                   enum_energy_efficiency_measurement_type[
+                                                       i] + '-' + enum_energy_saving_type[j]
+
+    return df[['subject', 'element_subject', 'device_subject', 'epc_date_before', 'epc_date'] +
               [x for x in df.columns if re.match("eem_.*_subject", x)] +
               [x for x in df.columns if re.match("measurement_.*", x)] +
               [x for x in df.columns if re.match("energy_saving_.*_subject", x)]]
@@ -111,14 +133,13 @@ def harmonize_eem_es(df, mapper, config):
         k, m = divmod(len(enum_energy_efficiency_measurement_type), parts_eem)
         eems_parted = [enum_energy_efficiency_measurement_type[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in
                        range(parts_eem)]
-        df_eem = clean_dataframe_eem_savings(df.copy(), eems_parted[chunk], start_column_eem)
         start_column_saving = 0
         for chunk_saving in range(parts_saving):
             k, m = divmod(len(enum_energy_saving_type), parts_saving)
             saving_parted = [enum_energy_saving_type[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in
                              range(parts_saving)]
             mapper.select_chunk(eems_parted[chunk], start_column_eem, saving_parted[chunk_saving], start_column_saving)
-            g = generate_rdf(mapper.get_mappings("eem_savings"), df_eem)
+            g = generate_rdf(mapper.get_mappings("eem_savings"), df)
             g.serialize('output.ttl', format='ttl')
             try:
                 save_rdf_with_source(g, config['source'], config['neo4j'])
@@ -154,70 +175,7 @@ def harmonize_data(data, **kwargs):
     save_rdf_with_source(g, config['source'], config['neo4j'])
 
     # EEM & Savings
-    harmonize_eem_es(df.copy(), mapper, config)
+    df_eem_es = clean_eem_savings(df.copy(), config['source'])
+    harmonize_eem_es(df_eem_es, mapper, config)
 
     # TS
-    freq = 'PT1Y'
-    neo4j_connection = config['neo4j']
-    neo = GraphDatabase.driver(**neo4j_connection)
-    hbase_conn2 = config['hbase_store_harmonized_data']
-
-    measured_property_list = ['EnergyConsumptionOil', 'EnergyConsumptionCoal',
-                              'EnergyConsumptionGas', 'EnergyConsumptionOthers',
-                              'EnergyConsumptionDistrictHeating',
-                              'EnergyConsumptionGridElectricity', 'EnergyConsumptionTotal']
-
-    measured_property_df = ['annual_energy_consumption_before_liquid_fuels',
-                            'annual_energy_consumption_before_hard_fuels',
-                            'annual_energy_consumption_before_gas', 'annual_energy_consumption_before_others',
-                            'annual_energy_consumption_before_heat_energy',
-                            'annual_energy_consumption_before_electricity',
-                            'annual_energy_consumption_before_total_consumption']
-
-    df['device_subject'] = df['subject'].apply(partial(device_subject, source=config['source']))
-
-    with neo.session() as session:
-        for i in range(len(measured_property_list)):
-            for index, row in df.iterrows():
-                device_uri = str(n[row['device_subject']])
-
-                sensor_id = sensor_subject(config['source'], row['subject'], measured_property_list[i], "RAW",
-                                           freq)
-                sensor_uri = str(n[sensor_id])
-                measurement_id = hashlib.sha256(sensor_uri.encode("utf-8"))
-                measurement_id = measurement_id.hexdigest()
-                measurement_uri = str(n[measurement_id])
-
-                create_sensor(session=session, device_uri=device_uri, sensor_uri=sensor_uri, unit_uri=units["KiloW-HR"],
-                              property_uri=bigg_enums[measured_property_list[i]],
-                              estimation_method_uri=bigg_enums.Naive,
-                              measurement_uri=measurement_uri, is_regular=True,
-                              is_cumulative=False, is_on_change=False, freq=freq, agg_func="SUM",
-                              dt_ini=pd.Timestamp(row['epc_date_before']),
-                              dt_end=pd.Timestamp(row['epc_date']), ns_mappings=settings.namespace_mappings)
-
-            reduced_df = df[[measured_property_df[i]]]
-
-            reduced_df['listKey'] = measurement_id
-            reduced_df['isReal'] = False
-            reduced_df['bucket'] = ((pd.to_datetime(df['epc_date_before']).values.astype(
-                int) // 10 ** 9) // settings.ts_buckets) % settings.buckets
-            reduced_df['start'] = (pd.to_datetime(df['epc_date_before']).values.astype(int)) // 10 ** 9
-            reduced_df['end'] = (pd.to_datetime(df['epc_date']).values.astype(int)) // 10 ** 9
-
-            reduced_df.rename(
-                columns={measured_property_df[i]: "value"},
-                inplace=True)
-
-            # device_table = f"harmonized_online_{measured_property_list[i]}_100_SUM_{freq}_{user}"
-            #
-            # save_to_hbase(reduced_df.to_dict(orient="records"), device_table, hbase_conn2,
-            #               [("info", ['end', 'isReal']), ("v", ['value'])],
-            #               row_fields=['bucket', 'listKey', 'start'])
-            #
-            # period_table = f"harmonized_batch_{measured_property_list[i]}_100_SUM_{freq}_{user}"
-
-            # save_to_hbase(reduced_df.to_dict(orient="records"), period_table, hbase_conn2,
-            #               [("info", ['end', 'isReal']), ("v", ['value'])],
-            #               row_fields=['bucket', 'start', 'listKey'])
-        print("finished")
