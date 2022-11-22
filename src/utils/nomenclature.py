71612f9d9dd906e934d9336892deaa4093d7c445
elskater98 <elskater98@gmail.com>
2022-11-22 08:19:20 +0100

    Fix bug: harmonized nomenclature mode value

--- src/utils/nomenclature.py
+++ src/utils/nomenclature.py
@@ -20,4 +20,4 @@ def raw_nomenclature(source, mode, data_type="", frequency="", user=""):
 def harmonized_nomenclature(mode, data_type, R, C, O, aggregation_function="", freq='', user=''):
     if not isinstance(mode, HarmonizedMode):
         raise Exception("Mode must be HarmonizedMode instance")
-    return f"harmonized_{mode}_{data_type}_{int(R)}{int(C)}{int(O)}_{aggregation_function}_{freq}_{user}"
+    return f"harmonized_{mode.value}_{data_type}_{int(R)}{int(C)}{int(O)}_{aggregation_function}_{freq}_{user}"
