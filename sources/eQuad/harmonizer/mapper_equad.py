217f9472c679bdb9a8f7e72ea847af882d6f5649
elskater98 <elskater98@gmail.com>
2022-12-20 10:51:13 +0100

    Calc project investment

--- sources/eQuad/harmonizer/mapper_equad.py
+++ sources/eQuad/harmonizer/mapper_equad.py
@@ -27,6 +27,32 @@ def calc_tarriff_avg(data):
     return mean(values) if values else None
 
 
+def calc_project_investment(row):
+    # Present Project Cost
+    engineeringAndDesignCosts = row.get('engineeringAndDesignCosts') if row.get('engineeringAndDesignCosts') else 0
+    equipmentCosts = row.get('equipmentCosts') if row.get('equipmentCosts') else 0
+    installationAndConstructionCosts = row.get('installationAndConstructionCosts') if row.get(
+        'installationAndConstructionCosts') else 0
+    salesAndBusinessDevelopmentCosts = row.get('salesAndBusinessDevelopmentCosts') if row.get(
+        'salesAndBusinessDevelopmentCosts') else 0
+    legalAndInsuranceCosts = row.get('legalAndInsuranceCosts') if row.get('legalAndInsuranceCosts') else 0
+    otherCosts = row.get('otherCosts') if row.get('otherCosts') else 0
+    markupCost = row.get('markupCost') if row.get('markupCost') else 0
+
+    calc_present_project_cost = pd.DataFrame(engineeringAndDesignCosts)['amount'].sum() + pd.DataFrame(equipmentCosts)[
+        'amount'].sum() + pd.DataFrame(installationAndConstructionCosts)['amount'].sum() + \
+                                pd.DataFrame(legalAndInsuranceCosts)['amount'].sum() + \
+                                pd.DataFrame(salesAndBusinessDevelopmentCosts)['amount'].sum() + \
+                                pd.DataFrame(otherCosts)['amount'].sum() / (1 - markupCost)
+
+    # Future Project Cost
+
+    calc_future_project_cost = pd.DataFrame(row.get('oneOffCosts'))['amount'].sum() if row.get('oneOffCosts') else 0
+    customer_down_payment = row.get('customerDownpayment') if row.get('customerDownpayment') else 0
+
+    return calc_present_project_cost + calc_future_project_cost + customer_down_payment
+
+
 def general_data(data):
     df = pd.DataFrame(data)
     df = df.applymap(decode_hbase)
@@ -87,9 +113,13 @@ def general_data(data):
     # Energy Efficiency Measure
     list_eem = []
     for index, row in df.iterrows():
+        print(row.get('engineeringAndDesignCosts'))
+        # OPEX = row['totalAuditCosts'] + row['totalEquipmentCost'] + row['totalInstallationCost'] + row[
+        #     'totalConstructionCost'] * (1 + row['markupCost'] / 100)
+        # CAPEX = 0
+        # eem_investment = OPEX + CAPEX
         for site in row['sites']:
             for ecms in site.get('ecms'):
-                print(ecms)
                 list_eem.append({"_id": "",
                                  "energyEfficiencyMeasureDescription": ecms.get('descriptionNew'),
                                  "energyEfficiencyMeasureInvestmentCurrency": row['hasProjectInvestmentCurrency'],
@@ -107,6 +137,7 @@ def general_data(data):
                                 )
 
     # TODO: energyEfficiencyMeasureType and energySavingType taxonomy
+
     print(list_eem)
 
     return df
